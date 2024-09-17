d4ec525f3d2d323323139866b4e8de659e083175
na0260 <samjr.008@gmail.com>
2024-09-18 04:42:44 +0600

    order page ui complete

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -2,8 +2,56 @@ import React from 'react';
 
 const OrdersPage = () => {
     return (
-        <div>
-            order page
+        <div className='p-4 lg:px-20 xl:px-40'>
+            <table className='w-full border-separate border-spacing-3'>
+                <thead>
+                    <tr className='text-left'>
+                        <th className='hidden md:block'>Order ID</th>
+                        <th>Date</th>
+                        <th>Price</th>
+                        <th className='hidden md:block'>Products</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className='text-sm md:text-base odd:bg-gray-100'>
+                        <td className='hidden md:block py-6 px-1'>1</td>
+                        <td >2021-01-01</td>
+                        <td className='py-6 px-1'>$10.00</td>
+                        <td className='hidden md:block py-6 px-1'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </td>
+                        <td className='py-6 px-1'>Completed</td>
+                    </tr>
+                    <tr className='text-sm md:text-base odd:bg-gray-100'>
+                        <td className='hidden md:block py-6 px-1'>1</td>
+                        <td >2021-01-01</td>
+                        <td className='py-6 px-1'>$10.00</td>
+                        <td className='hidden md:block py-6 px-1'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </td>
+                        <td className='py-6 px-1'>Completed</td>
+                    </tr>
+                    <tr className='text-sm md:text-base odd:bg-gray-100'>
+                        <td className='hidden md:block py-6 px-1'>1</td>
+                        <td >2021-01-01</td>
+                        <td className='py-6 px-1'>$10.00</td>
+                        <td className='hidden md:block py-6 px-1'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </td>
+                        <td className='py-6 px-1'>Completed</td>
+                    </tr>
+                    <tr className='text-sm md:text-base odd:bg-gray-100'>
+                        <td className='hidden md:block py-6 px-1'>1</td>
+                        <td >2021-01-01</td>
+                        <td className='py-6 px-1'>$10.00</td>
+                        <td className='hidden md:block py-6 px-1'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </td>
+                        <td className='py-6 px-1'>Completed</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
