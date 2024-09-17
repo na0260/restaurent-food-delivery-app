aaadf2727b6a165324f22e6adc2e22e91df396d3
na0260 <samjr.008@gmail.com>
2024-09-17 20:27:04 +0600

    Notification component ui complete

--- src/app/components/Notification.jsx
+++ src/app/components/Notification.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Notification = () => {
     return (
-        <div>
-            Notification
+        <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </div>
     );
 };
