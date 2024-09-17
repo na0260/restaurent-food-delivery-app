5f2e6964d4b0dc45585fc03dd271d0070ba3ea5e
na0260 <samjr.008@gmail.com>
2024-09-17 20:50:36 +0600

    Mobile menu component created

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Link from "next/link";
+import Menu from "@/app/components/Menu";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+            {/*Logo*/}
+            <div className='text-xl'>
+                <Link href="/">NA CAFE</Link>
+            </div>
+            {/*Mobile Menu*/}
+            <div>
+                <Menu/>
+            </div>
         </div>
     );
 };
